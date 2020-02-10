bf70ad6cd3d02bfc74c62034186f39e4037b5e85
dvtate <dtesta1@hawk.iit.edu>
2020-02-09 19:54:22 -0600

    forgot to run tests :/

--- mps/02/mytr.c
+++ mps/02/mytr.c
@@ -9,28 +9,43 @@ int main(int argc, char** argv) {
 		printf("       %s -d STRING\n", argv[0]);
 		exit(1);
 	}
-	
-	const char* from = argv[1];
-	const unsigned int terms = strlen(from);
 
-	const char* to = argv[2];
+
+	char delete_mode;
+	char* from = argv[1];
+	char* to = argv[2];
+
+	if (strcmp(from, "-d") == 0) {
+		from = to;
+		delete_mode = 1;
+	} else if (strcmp(to, "-d") == 0) {
+		delete_mode = 1;
+	} else {
+		delete_mode = 0;
+	}
+
+	const unsigned int terms = strlen(from);
 
 	if (strlen(to) != strlen(from)) {
 		printf("STRING1 and STRING2 must have the same length\n");
 		exit(1);
 	}
-	
+
 	char c;
+	unsigned int i;
 	while ((c = fgetc(stdin)) != EOF) {
-		for (unsigned int i = 0; i < terms; i++)
+		for (i = 0; i < terms; i++)
 			if (from[i] == c) {
 				c = to[i];
+				if (!delete_mode)
+					fputc(c, stdout);
 				break;
 			}
-
-		fputc(c, stdout);
+		if (i == terms) 
+			fputc(c, stdout);
 	}
 
 
 	return 0;
 }
+
